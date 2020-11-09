1a952e86697b9314298ad623a7614b4e91c684f8
cKassimiro <gustavo.cassimiro26@gmail.com>
2020-11-09 20:41:42 -0300

    subindo maquina virtual Nagrant(vagrant + nodejs)

--- nig/site/node_4_DB/app/database.js
+++ nig/site/node_4_DB/app/database.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
     user     : 'root',
-    password : '0v4T5Ug64!',
+    password : '0v4T5Ug64!', // 'nagrant' or '0v4T5Ug64!'
     database : 'sensor'
 });
 
