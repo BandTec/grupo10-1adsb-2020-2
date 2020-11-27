cd45a4c2d9c85d1f8dfaf84345ae5d567abde307
cKassimiro <gustavo.cassimiro26@gmail.com>
2020-11-26 22:49:40 -0300

    api de insert gerando valores de outras glebas

--- nig/api's/api_insert/api-aquisicao-arqcomp/app/controller.js
+++ nig/api's/api_insert/api-aquisicao-arqcomp/app/controller.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { ArduinoDataTemp } = require("./newserial");
-const { ArduinoDataHumidity } = require("./serialHumidity");
 const db = require("./database");
 const router = express.Router();
 
@@ -21,42 +20,25 @@ router.get("/", (request, response, next) => {
   });
 });
 
-router.get("/humidity", (request, response, next) => {
-  let sum = ArduinoDataHumidity.List.reduce((a, b) => a + b, 0);
-  let average = (sum / ArduinoDataHumidity.List.length).toFixed(2);
-  let sumHour = ArduinoDataHumidity.ListHour.reduce((a, b) => a + b, 0);
-  let averageHour = (sumHour / ArduinoDataHumidity.ListHour.length).toFixed(2);
-
-  response.json({
-    data: ArduinoDataHumidity.List,
-    total: ArduinoDataHumidity.List.length,
-    average: isNaN(average) ? 0 : average,
-    dataHour: ArduinoDataHumidity.ListHour,
-    totalHour: ArduinoDataHumidity.ListHour.length,
-    averageHour: isNaN(averageHour) ? 0 : averageHour,
-  });
-});
-
 router.get("/sendData", (request, response) => {
-  let teste = Math.random()* 45;
+
   let temperature = ArduinoDataTemp.List[ArduinoDataTemp.List.length - 1];
-  let Humidity = ArduinoDataHumidity.List[ArduinoDataHumidity.List.length - 1];
 
   console.log(temperature);
-  console.log(teste);
-  console.log(Humidity);
+
 
   db.conectar()
     .then(() => {
       const sql = `
         INSERT into Dados_sensor(temperatura, umidade, horario_captacao)
-        values (${Math.trunc(temperature[0])}, ${Math.trunc(temperature[1])}, '${agora()}');`;
-      /*INSERT into Dados_sensor (temperatura, umidade, horario_captacao, id_dados)
-      values (${temperature-10}, ${Humidity+20}, '${agora()}', 2);
-      INSERT into Dados_sensor (temperatura, umidade, horario_captacao, id_dados)
-      values (${temperature+5}, ${Humidity-20}, '${agora()}', 3);
-      INSERT into Dados_sensor (temperatura, umidade, horario_captacao, id_dados)
-      values (${temperature-5}, ${Humidity-20}, '${agora()}', 4);*/
+        values (${Math.trunc(temperature[0])}, ${Math.trunc(temperature[1])}, '${agora()}')
+        INSERT into Dados_sensor (temperatura, umidade, horario_captacao)
+        values (${Math.trunc(temperature[0])*0.2}, ${Math.trunc(temperature[1])*0.4}, '${agora()}');
+        INSERT into Dados_sensor (temperatura, umidade, horario_captacao)
+        values (${Math.trunc(temperature[0])*0.7+3}, ${Math.trunc(temperature[1])*1.3}, '${agora()}');
+        INSERT into Dados_sensor (temperatura, umidade, horario_captacao)
+        values (${Math.trunc(temperature[0])*0.2+2}, ${Math.trunc(temperature[1])*0.3}, '${agora()}');`
+
       console.log(sql);
       return db.sql.query(sql).then(() => {
         console.log("Registro inserido com sucesso! \n");
