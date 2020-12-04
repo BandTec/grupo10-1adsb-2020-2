79cac1c7191d5932b8d7277aea17540470c807fb
cKassimiro <gustavo.cassimiro26@gmail.com>
2020-12-04 17:48:45 -0300

    separando graficos em duas paginas (graph_frame_temp e graph_frame_umid)/ adicionando animação quando gleba for selecionada na tabela

--- nig/api's/api_select/public3/js/frames.js
+++ nig/api's/api_select/public3/js/frames.js
@@ -9,27 +9,48 @@ function change_frame(local) {
         
         home.classList.add('atual')
         graph.classList.remove('atual')
+        graph_umid.classList.remove('atual')
         config.classList.remove('atual')
         help.classList.remove('atual')
         
         // ALTERAR ICONES SELECIONADOS
         home_icon.src = '../images/home-icon-red.png';
         graph_icon.src = '../images/chart-green.png';
+        graph_icon_umid.src = '../images/chart-green.png';
+        sett_icon.src = '../images/settings-green.png';
+        help_icon.src = '../images/help-green.png';    
+    } else if(local == 'graph_umid'){
+frame.src = '../dashboard/iframes/graph_frame_umid.html'
+        
+        // ALTERAR IFRAMES
+        
+        home.classList.remove('atual')
+        graph.classList.remove('atual')
+        graph_umid.classList.add('atual')
+        config.classList.remove('atual')
+        help.classList.remove('atual')
+        
+        // ALTERAR ICONES SELECIONADOS
+        home_icon.src = '../images/home-icon.png';
+        graph_icon.src = '../images/chart-green.png';
+        graph_icon_umid.src = '../images/chart-red.png';
         sett_icon.src = '../images/settings-green.png';
         help_icon.src = '../images/help-green.png';    
     } else if (local == 'graph'){
-    frame.src = '../dashboard/iframes/graph_frame.html'
+    frame.src = '../dashboard/iframes/graph_frame_temp.html'
         
         // ALTERAR IFRAMES
         
         home.classList.remove('atual')
         graph.classList.add('atual')
+        graph_umid.classList.remove('atual')
         config.classList.remove('atual')
         help.classList.remove('atual')
         
         // ALTERAR ICONES SELECIONADOS
         home_icon.src = '../images/home-icon.png';
         graph_icon.src = '../images/chart-red.png';
+        graph_icon_umid.src = '../images/chart-green.png';
         sett_icon.src = '../images/settings-green.png';
         help_icon.src = '../images/help-green.png';    
     } else if (local == 'config'){
@@ -39,12 +60,14 @@ function change_frame(local) {
         
         home.classList.remove('atual')
         graph.classList.remove('atual')
+        graph_umid.classList.remove('atual')
         config.classList.add('atual')
         help.classList.remove('atual')
         
         // ALTERAR ICONES SELECIONADOS
         home_icon.src = '../images/home-icon.png';
         graph_icon.src = '../images/chart-green.png';
+        graph_icon_umid.src = '../images/chart-green.png';
         sett_icon.src = '../images/settings-red.png';
         help_icon.src = '../images/help-green.png';    
     } else if (local == 'help'){
@@ -54,12 +77,14 @@ function change_frame(local) {
         
         home.classList.remove('atual')
         graph.classList.remove('atual')
+        graph_umid.classList.remove('atual')
         config.classList.remove('atual')
         help.classList.add('atual')
         
         // ALTERAR ICONES SELECIONADOS
         home_icon.src = '../images/home-icon.png';
         graph_icon.src = '../images/chart-green.png';
+        graph_icon_umid.src = '../images/chart-green.png';
         sett_icon.src = '../images/settings-green.png';
         help_icon.src = '../images/help-red.png';    
     } else {
