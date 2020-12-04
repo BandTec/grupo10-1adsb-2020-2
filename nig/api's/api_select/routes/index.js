7b498d1c8a34b1bbdf834def8d2017882ca2ac02
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-12-03 22:19:22 -0200

    começo das correções dos erros de insert do form de contato

--- nig/api's/api_select/routes/index.js
+++ nig/api's/api_select/routes/index.js
@@ -1,33 +1,11 @@
 var express = require('express');
-const contato = require('../models').Contato;
+const contato = require('../models/contato');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// INSERÇÃO DO CONTATO 
 
-router.post('/contato', function(req, res, next){
-
-  console.log('Inserindo comentario \n');
-
-  contato.create({
-    email: req.body.email_cont,
-    nome: req.body.nome_cont,
-    mensagem: req.body.mensagem_cont 
-  }).then(resultado =>{
-
-    res.send(console.log("Comentário gerando com sucesso!"));
-    
-    res.send(resultado);
-
-  }).catch(erro =>{
-
-    res.send("Erro: falha no envio do comentário" + erro);
-
-    res.status(666).send(erro.message);
-  })
-});
 module.exports = router;
