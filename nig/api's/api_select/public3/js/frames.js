aa6eda5cd9e5405c33938d5006690286e1bd6875
cKassimiro <gustavo.cassimiro26@gmail.com>
2020-12-08 22:42:28 -0300

    Revert "home dashboard com botões de redirecionamento"
    This reverts commit 55a3021ccd14fc9ba9afbb4da166d9d5e95166c7.

--- nig/api's/api_select/public3/js/frames.js
+++ nig/api's/api_select/public3/js/frames.js
@@ -2,14 +2,11 @@ function change_frame(local) {
   frame = document.getElementById("frame_change");
 
   if (local == "home") {
-    
-    frame.src = "../dashboard/dash.html";
+    frame.src = "../dashboard/iframes/index_frame.html";
 
     // ALTERAR IFRAMES
 
-    home_index.classList.add("home_index_block");
-    home_index.classList.remove("home_index_none")
-    
+    home.classList.add("atual");
     graph.classList.remove("atual");
     graph_umid.classList.remove("atual");
 
@@ -20,16 +17,12 @@ function change_frame(local) {
     graph_icon.src = "../images/chart-temp-green.png";
     graph_icon_umid.src = "../images/chart-umid-green.png";
     help_icon.src = "../images/help-green.png";
-    
   } else if (local == "graph_umid") {
-    
     frame.src = "../dashboard/iframes/graph_frame_umid.html";
 
     // ALTERAR IFRAMES
 
-    home_index.classList.remove("home_index_block");
-    home_index.classList.add("home_index_none")
-    
+    home.classList.remove("atual");
     graph.classList.remove("atual");
     graph_umid.classList.add("atual");
 
@@ -40,16 +33,12 @@ function change_frame(local) {
     graph_icon.src = "../images/chart-temp-green.png";
     graph_icon_umid.src = "../images/chart-umid-red.png";
     help_icon.src = "../images/help-green.png";
-    
   } else if (local == "graph") {
-    
     frame.src = "../dashboard/iframes/graph_frame_temp.html";
 
     // ALTERAR IFRAMES
 
-    home_index.classList.remove("home_index_block");
-    home_index.classList.add("home_index_none")
-    
+    home.classList.remove("atual");
     graph.classList.add("atual");
     graph_umid.classList.remove("atual");
 
@@ -60,16 +49,12 @@ function change_frame(local) {
     graph_icon.src = "../images/chart-temp-red.png";
     graph_icon_umid.src = "../images/chart-umid-green.png";
     help_icon.src = "../images/help-green.png";
-    
   } else if (local == "help") {
-    
     frame.src = "../dashboard/iframes/help_frame.html";
 
     // ALTERAR IFRAMES
 
-    home_index.classList.remove("home_index_block");
-    home_index.classList.add("home_index_none")
-    
+    home.classList.remove("atual");
     graph.classList.remove("atual");
     graph_umid.classList.remove("atual");
 
--- nig/api's/api_select/public3/js/redirecionar_home.js
+++ nig/api's/api_select/public3/js/redirecionar_home.js
@@ -1,105 +1,38 @@
-  
-function redirecionar_gleba(tipo_grafico, gleba ) {
+function redirecionar_gleba(tipo_grafico, gleba) {
 
-var frame = document.getElementById('frame_change')
+    var frame = document.getElementById('frame_change')
 
   if (tipo_grafico == "temp" && gleba == "oeste") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_temp.html';
-      
-      home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-red.png";
-    graph_icon_umid.src = "../images/chart-umid-green.png";
-    help_icon.src = "../images/help-green.png";
 
-  
+      frame.src = '../dashboard/iframes/graph_frame_temp.html';
+
   } else if (tipo_grafico == "umid" && gleba == "oeste") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_umid.html';
-    
-    home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-green.png";
-    graph_icon_umid.src = "../images/chart-umid-red.png";
-    help_icon.src = "../images/help-green.png";
-  
-  } else if (tipo_grafico == "temp" && gleba == "sul") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_temp.html';
-          
-          home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-red.png";
-    graph_icon_umid.src = "../images/chart-umid-green.png";
-    help_icon.src = "../images/help-green.png";
-  
-  } else if (tipo_grafico == "umid" && gleba == "sul") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_umid.html';
-    
-    home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-green.png";
-    graph_icon_umid.src = "../images/chart-umid-red.png";
-    help_icon.src = "../images/help-green.png";
-  
-  } else if (tipo_grafico == "temp", gleba == "leste") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_temp.html';
-      
-    
-    home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-green.png";
-    graph_icon_umid.src = "../images/chart-umid-red.png";
-    help_icon.src = "../images/help-green.png";
-  
-  } else if (tipo_grafico == "umid", gleba == "leste") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_umid.html';
-        
-        home_icon.src = "../images/home-icon.png";
-        graph_icon.src = "../images/chart-temp-green.png";
-        graph_icon_umid.src = "../images/chart-umid-red.png";
-        help_icon.src = "../images/help-green.png";
-  
-  } else if (tipo_grafico == "temp" && gleba == "norte") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
-      frame.src = '../dashboard/iframes/graph_frame_temp.html';
-      
-      home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-red.png";
-    graph_icon_umid.src = "../images/chart-umid-green.png";
-    help_icon.src = "../images/help-green.png";
 
-  
-  } else if (tipo_grafico == "umid" && gleba == "norte") {
-    
-      home_index.classList.remove("home_index_block");
-      home_index.classList.add("home_index_none")
-      
       frame.src = '../dashboard/iframes/graph_frame_umid.html';
-    
-    home_icon.src = "../images/home-icon.png";
-    graph_icon.src = "../images/chart-temp-green.png";
-    graph_icon_umid.src = "../images/chart-umid-red.png";
-    help_icon.src = "../images/help-green.png";
+
+  } else if (tipo_grafico == "temp" && gleba == "sul") {
+
+      frame.src = '../dashboard/iframes/graph_frame_temp.html';
+
+  } else if (tipo_grafico == "umid" && gleba == "sul") {
+
+      frame.src = '../dashboard/iframes/graph_frame_umid.html';
+
+  } else if ((tipo_grafico == "temp", gleba == "leste")) {
+
+      frame.src = '../dashboard/iframes/graph_frame_temp.html';
+
+  } else if ((tipo_grafico == "umid", gleba == "leste")) {
+
+      frame.src = '../dashboard/iframes/graph_frame_umid.html';
+
+  } else if (tipo_grafico == "temp" && gleba == "norte") {
+
+      frame.src = '../dashboard/iframes/graph_frame_temp.html';
+
+  } else if (tipo_grafico == "umid" && gleba == "norte") {
+
+      frame.src = '../dashboard/iframes/graph_frame_umid.html';
+
   }
 }
